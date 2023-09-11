25c8f60879953ad4a6b9ca499e2d0f25a1e1d36a
Khamzauly <104725447+Khamzauly@users.noreply.github.com>
2023-09-11 16:42:41 +0600

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,19 +1,26 @@
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+import logging
 import os
 
+# Инициализация логгера
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Токен вашего бота
 TOKEN = os.getenv("TOKEN")
 
 tasks = {
     "Task 1": {"status": "Not Done", "worker": None},
     "Task 2": {"status": "Not Done", "worker": None},
-    # ...
+    # Добавьте дополнительные задачи по необходимости
 }
 
 def start(update: Update, _: CallbackContext) -> None:
     keyboard = []
-    for task in tasks:
-        keyboard.append([InlineKeyboardButton(task, callback_data=task)])
+    for task, info in tasks.items():
+        keyboard.append([InlineKeyboardButton(f"{task} - {info['status']}", callback_data=task)])
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text("Choose a task:", reply_markup=reply_markup)
 
@@ -24,10 +31,15 @@ def button(update: Update, _: CallbackContext) -> None:
     tasks[task]["worker"] = query.from_user.id
     query.edit_message_text(text=f"{task} is now Done by {query.from_user.id}")
 
-updater = Updater(TOKEN)
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
 
-updater.dispatcher.add_handler(CommandHandler("start", start))
-updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CallbackQueryHandler(button))
 
-updater.start_polling()
-updater.idle()
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
