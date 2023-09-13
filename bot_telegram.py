65286348d057517e73e52c75ab426fd4f9c48d16
Khamzauly <104725447+Khamzauly@users.noreply.github.com>
2023-09-13 22:46:12 +0600

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,40 +1,37 @@
-import datetime
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
+from datetime import datetime
 
-# Настройка Google Sheets API
+# Инициализация Google Sheets API
+json_file_path = 'client_secret_73898426089-3lfiu34v8g4o3lda3r51qonm6mj0hpnr.apps.googleusercontent.com.json'
 scopes = ['https://www.googleapis.com/auth/spreadsheets']
-credentials = Credentials.from_service_account_file('client_secret_73898426089-3lfiu34v8g4o3lda3r51qonm6mj0hpnr.apps.googleusercontent.com.json', scopes=scopes)
+credentials = Credentials.from_service_account_file(json_file_path, scopes=scopes)
 service = build('sheets', 'v4', credentials=credentials)
 sheet = service.spreadsheets()
 
-# Получение данных из Google Sheets
+# Функция для получения задач
 def get_tasks():
-    today = datetime.datetime.now().strftime("%d.%m.%y")
-    range_name = f'{today}!A2:D'
-    result = sheet.values().get(spreadsheetId="YOUR_SPREADSHEET_ID", range=range_name).execute()
+    result = sheet.values().get(spreadsheetId="1xjphW6Zlc3Hx73h2pTmFgDLeR4-MhVw2xITgjIOLN4w", range="13.09.23!A2:D10").execute()
     return result.get('values', [])
 
-# Обновление данных в Google Sheets
+# Функция для обновления задач
 def update_task(row, user_name):
-    today = datetime.datetime.now().strftime("%d.%m.%y")
-    range_name = f'{today}!B{row}:D{row}'
-    values = [
-        ["TRUE", user_name, datetime.datetime.now().strftime("%H:%M:%S")]
-    ]
-    body = {'values': values}
-    sheet.values().update(spreadsheetId="1xjphW6Zlc3Hx73h2pTmFgDLeR4-MhVw2xITgjIOLN4w", range=range_name, valueInputOption="RAW", body=body).execute()
+    sheet.values().update(
+        spreadsheetId="1xjphW6Zlc3Hx73h2pTmFgDLeR4-MhVw2xITgjIOLN4w",
+        range=f"13.09.23!B{row}:D{row}",
+        body={"values": [[True, user_name, str(datetime.now())]]},
+        valueInputOption="RAW"
+    ).execute()
 
 # Обработчик команды /start
 def start(update: Update, context: CallbackContext):
     keyboard = []
     tasks = get_tasks()
     for i, task in enumerate(tasks):
-        status = '✅' if task[1] == "TRUE" else ''
-        name = task[2] if len(task) > 2 else ''
-        keyboard.append([InlineKeyboardButton(f"{task[0]} {status} {name}", callback_data=str(i))])
+        status = "✅" if task[1] == "TRUE" else "❌"
+        keyboard.append([InlineKeyboardButton(f"{task[0]} {status}", callback_data=str(i))])
     
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Выберите задачу:', reply_markup=reply_markup)
@@ -43,15 +40,17 @@ def start(update: Update, context: CallbackContext):
 def button(update: Update, context: CallbackContext):
     query = update.callback_query
     task_index = int(query.data)
+    tasks = get_tasks()
     user_name = update.effective_user.first_name
     
     # Обновление данных в Google Sheets
-    update_task(task_index + 2, user_name)  # task_index + 2, потому что нумерация строк в Google Sheets начинается с 1, и у нас есть заголовок.
+    update_task(task_index + 2, user_name)
     
     # Здесь отправьте обновленный список задач всем пользователям
 
 # Основной код
 updater = Updater("6655324353:AAEWkQb0b971nP4kf6OvS5s6fof0-NNfKHA")
+
 dp = updater.dispatcher
 
 dp.add_handler(CommandHandler("start", start))
