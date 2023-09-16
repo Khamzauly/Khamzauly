0ede452e75619e01c1dfac7416c7488dd0c26303
Khamzauly <104725447+Khamzauly@users.noreply.github.com>
2023-09-17 02:06:27 +0600

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -56,9 +56,12 @@ chat_names = {}
 
 def load_chat_names():
     global chat_names
+    chat_names = {}  # Обнуляем переменную
     result = sheet.values().get(spreadsheetId="1xjphW6Zlc3Hx73h2pTmFgDLeR4-MhVw2xITgjIOLN4w", range="чаты!A:B").execute()
     values = result.get('values', [])
     chat_names = {row[1]: row[0] for row in values if len(row) > 1}
+
+    
 
 
 def start(update: Update, context: CallbackContext):
