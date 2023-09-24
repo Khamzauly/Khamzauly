bf5f4f70cccc0ff8849ebb4af650bdfd663c72b3
Khamzauly <104725447+Khamzauly@users.noreply.github.com>
2023-09-25 00:27:00 +0600

    Create handlers.py

---
+++ handlers.py
@@ -0,0 +1,8 @@
+from telegram import Update, CallbackContext
+from telegram.ext import CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+from cleaning_regulations import start, button, photo
+
+# Обработчики
+start_handler = CommandHandler("start", start)
+button_handler = CallbackQueryHandler(button)
+photo_handler = MessageHandler(Filters.photo, photo)
